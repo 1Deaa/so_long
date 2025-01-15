072624cd0e4d887b660975fcbf7376ec06168c2e
drahwanj <deaarahwnji@gmail.com>
2025-01-16 02:54:27 +0300

    Added flood_fill algorithm to check if there's a valid path

---
+++ src/map/flood_fill.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 22:41:47 by drahwanj          #+#    #+#             */
+/*   Updated: 2025/01/15 22:41:47 by drahwanj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	flood_fill(t_map *map, char **map_dup)
+{
+	t_map	flood;
+
+	flood = *map;
+	flood.matrix = map_dup;
+	flood_fill_loop(&flood, flood.player_x, flood.player_y);
+	map->collectible = flood.collectible;
+	map->exit = flood.exit;
+}
---
+++ src/map/flood_fill_util.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill_util.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 00:41:56 by drahwanj          #+#    #+#             */
+/*   Updated: 2025/01/16 00:41:57 by drahwanj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	flood_fill_loop(t_map *flood, int x, int y)
+{
+	if (flood->matrix[x][y] == '1' || flood->matrix[x][y] == 'F')
+		return ;
+	if (flood->matrix[x][y] == 'E')
+		flood->exit = 0;
+	if (flood->matrix[x][y] == 'C')
+		flood->collectible--;
+	flood->matrix[x][y] = 'F';
+	if (x > 0)
+		flood_fill_loop(flood, x - 1, y);
+	if (x < flood->rows)
+		flood_fill_loop(flood, x + 1, y);
+	if (y > 0)
+		flood_fill_loop(flood, x, y - 1);
+	if (y < flood->cols - 1)
+		flood_fill_loop(flood, x, y + 1);
+}
+
+char	**duplicate_map(char **map, int rows)
+{
+	int		i;
+	char	**dup;
+
+	dup = (char **)malloc(sizeof(char *) * (rows + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < rows)
+	{
+		dup[i] = ft_strdup(map[i]);
+		i++;
+	}
+	dup[i] = NULL;
+	return (dup);
+}
+
+void	free_matrix(char **matrix, int rows)
+{
+	int	i;
+
+	i = 0;
+	while (i < rows)
+	{
+		free(matrix[i]);
+		i++;
+	}
+	free(matrix);
+}
